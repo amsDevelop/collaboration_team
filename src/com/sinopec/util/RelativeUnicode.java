e40643655cd42084cbbb2dbdbb36ec9639316645
mandy <gaolixiao1233@126.com>
2014-08-30 11:46:12 +0800

    unicode

--- src/com/sinopec/util/RelativeUnicode.java
+++ src/com/sinopec/util/RelativeUnicode.java
@@ -1,9 +1,20 @@
 package com.sinopec.util;
 
 public class RelativeUnicode {
-    
-	public static final String haixiang = "4294967296";
-	public static final String tansuanyanyan = "281474976710656";
-	public static final String gushengjie="4096";
+	
+	public static final String haixiang = "281473902968832";//海相 沉积相
+	public static final String tansuanyanyan = "1095216660480";//储层岩性 碳酸盐岩
+	public static final String qianhaiwuxi = "1023";//前寒武系
+	public static final String zhiliuxi = "31457280";//志留系
+	public static final String nipenxi = "1006632960";//泥盆系
+	public static final String shitanxi = "7516192768"; //石炭系
+	public static final String erdiexi = "128849018880"; //二叠系
+	public static final String sandiexi = "4123168604160"; //三叠系
+	public static final String zhuluoxi = "65970697666560"; //侏罗系
+	public static final String baiyaxi = "492581209243648"; //白垩系
+	public static final String gujinxi = "492581209243648"; //古近系
+	public static final String xinjinxi = "558446353793941504"; //新近系
+	
+	
 //	public static final String qianwuhaixi="";
 }
